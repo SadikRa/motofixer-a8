baf9c7ed2cf7fb463b050d9958b890b3267d3277
SadikRa <sadikrahman494@gmail.com>
2025-04-17 20:54:43 +0600

    server setup done

---
+++ src/app/routes/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+const router = express.Router();
+
+const moduleRoutes = [
+  {},
+  // {
+  //   path: "/user",
+  //   route: userRoutes,
+  // },
+  // {
+  //   path: "/admin",
+  //   route: adminRoutes,
+  // },
+  // {
+  //   path: "/auth",
+  //   route: authRoutes,
+  // },
+];
+
+// moduleRoutes.forEach((route) => router.use(route.path, route.route));
+
+export default router;
